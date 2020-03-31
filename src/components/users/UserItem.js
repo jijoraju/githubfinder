7026e8c11e0eb9499a8bc98a3bd6a3b0a14b1133
kvyaf ekm <kvyafekm@gmail.com>
2020-03-31 20:47:03 +0530

    user component

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 
 export class UserItem extends Component {
-  state = {
-    id: 'id',
-    login: 'Jijo',
-    avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-    html_url: 'https://github.com/jijoraju'
-  };
   render() {
     return (
       <div className='card text-center'>
-        <img src={this.state.avatar_url} alt='' style={{ width: '60px' }} />
-        <h3>{this.state.login}</h3>
-        <a href={this.state.html_url} className='btn btn-dark btn-sm my-1'>
+        <img
+          src={this.props.user.avatar_url}
+          alt=''
+          style={{ width: '60px' }}
+        />
+        <h3>{this.props.user.login}</h3>
+        <a href={this.props.user.html_url} className='btn btn-dark btn-sm my-1'>
           More
         </a>
       </div>
