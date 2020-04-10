3371db496351459ed783b5f53ea53c70357ce77c
Jijo Raju (JR) <jijoraju11@gmail.com>
2020-04-10 20:26:44 +0530

    axios added and github api called

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/layouts/Navbar';
 import User from './components/users/User';
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get('https://api.github.com/users');
+    this.setState({ users: res.data, loading: false });
+  }
+
   render() {
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <User />
+          <User users={this.state.users} loading={this.state.loading} />
         </div>
       </div>
     );
